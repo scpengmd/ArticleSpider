7c5d6f42ea6ff53a59f59a99bf94c7c7e2dcac85
scpengmd <pengmd@shuchidt.com>
2017-11-28 14:21:46 +0800

    item,test.py

--- ArticleSpider/test.py
+++ ArticleSpider/test.py
@@ -5,3 +5,4 @@ project_dir = os.path.abspath(os.path.dirname(__file__))
 IMAGES_STORE = os.path.join(project_dir, 'images')
 b = 'C:\\Users\\leo\\Documents\\ArticleSpider\\ArticleSpider\\images'
 print(IMAGES_STORE)
+#cffcf
